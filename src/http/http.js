f4cd696fd9b0c33c7abe9bc6384fbff33db72df6
psevdomaksim <varmax2004@gmail.com>
2024-10-04 22:03:06 +0300

    Add new server url

--- src/http/http.js
+++ src/http/http.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const $host = axios.create({
-  baseURL: "https://stage-5-1-server.vercel.app",
+  baseURL: "https://stage-5-2-server.vercel.app",
   withCredentials: true,
   //baseURL: "http://localhost:5000",
 });
